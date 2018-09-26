275b6fa3060764e18a498f92ddda9e7ac17195e0
thomas.comes <thomas.comes@matters.tech>
2018-09-26 18:27:04 +0200

    Added giphy search

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-(function () {})();
+(function() {
+  var giphyAPIKey = "aARVN3UFC5LgJpeVNDKMxkzjN2B4Zu9n";
+  var ul = $("#ul");
+  var searchInput = $("#searchInput");
+  var submitButton = $("#submitButton");
+
+  function getGifs() {
+    console.log("coucou");
+    $.get(
+      "http://api.giphy.com/v1/gifs/search",
+      {
+        api_key: giphyAPIKey,
+        q: searchInput.val()
+      },
+      function(data) {
+        displayGifs(data.data);
+        console.log(data);
+      }
+    );
+  }
+
+  function displayGifs(gifs) {
+    gifs.forEach(function(gif) {
+      var li = $('<li class="list-inline-item"></li>');
+      var img = $('<img src="' + gif.images.original.url + '" >');
+      li.append(img);
+      ul.append(li);
+    });
+  }
+
+  submitButton.click(getGifs);
+})();
